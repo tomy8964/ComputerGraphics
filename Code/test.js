d6024525c9976bfb1fcdcba1a8af9da66ef6d92a
Seong-guk Kim <54500840+kjk7212@users.noreply.github.com>
2022-10-30 23:32:29 +0900

    prototype
    Clicking on a piece will fly in the direction you are currently viewing/ It will gradually become transparent/ If there is only one piece left, it will rotate and display.

--- Code/test.js
+++ Code/test.js
@@ -5,23 +5,19 @@ var program;
 var scene;
 var renderer;
 var loader = new THREE.GLTFLoader();
-var check =false;
 var camera;
 
 raycaster = new THREE.Raycaster();
 mouse = new THREE.Vector2();
 
-const dalgona=[
-    loadRec(1),
-    loadRec(2),
-    loadRec(3),
-    loadRec(4),
-    loadRec(5),
-    loadRec(6),
-    loadRec(7),
-    loadRec(9),
-    loadRec(10)
-];
+var rectangle = [];
+var mainShapeObject;
+
+var animationObject = [];
+var animationDestinationVector = [];
+var animationMovingVector = [];
+
+var alreadyIn = false;
 
 window.onload = function init(){
 	const canvas = document.getElementById( "gl-canvas" );
@@ -36,7 +32,7 @@ window.onload = function init(){
 	scene.background = new THREE.Color(0x000000);
 
 
-	camera = new THREE.PerspectiveCamera(75,canvas.width / canvas.height,0.1, 100);
+	camera = new THREE.PerspectiveCamera(75,canvas.width / canvas.height,0.1, 1000);
 	camera.position.y = 30;
     camera.rotation.x = -1.5;
 
@@ -46,26 +42,65 @@ window.onload = function init(){
     const controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.update();
 
+    //If you want to test other shapes, you can choose the load function you want
+    
+    //loadPolygon();
+    //loadTriangle(); need modify 
+    loadSquare();
+
     animate();
 }
 
-function animate(time) {
-    time *= 0.001;
+function animate() {
     renderer.render(scene,camera);
+    moveAnimation();
+    rotateAnimation();
+    fadeOutAnimation();
+    if(rectangle.length == 1)finallAnimation();
     requestAnimationFrame(animate);
 }
 
-function loadRec(num){
-    var url = '../Graphic/model/rec*.gltf'
-	    loader.load(url.replace('*',String(num)), function(gltf){
-	        gltf.scene.children[0].scale.set(10,10,10);
-	        scene.add(gltf.scene);
-            if(num<8)dalgona[num-1]=gltf;
-            else dalgona[num-2]=gltf;
-            if(num==10)check = true;
-	    },  undefined, function (error) {
-            console.log(error);
-	});
+function loadSquare(){
+    for(i=1; i<10; i++){
+        var url = '../Graphic/model/rec*.gltf'
+	        loader.load(url.replace('*',String(i)), function(gltf){
+	            gltf.scene.children[0].scale.set(10,10,10);
+	            scene.add(gltf.scene);
+                rectangle.push(gltf.scene.children[0].id);
+                mainShapeObject = gltf.scene.children[0];
+	        },  undefined, function (error) {
+                console.log(error);
+	    });
+    }
+}
+
+function loadTriangle(){
+    for(i=1; i<5; i++){
+        var url = '../Graphic/model/sugartri*.gltf'
+	        loader.load(url.replace('*',String(i)), function(gltf){
+	            gltf.scene.children[0].scale.set(10,10,10);
+	            scene.add(gltf.scene);
+                rectangle.push(gltf.scene.children[0].id);
+                mainShapeObject = gltf.scene.children[0];
+	        },  undefined, function (error) {
+                console.log(error);
+	    });
+    }
+}
+
+function loadPolygon(){
+    for(i=1; i<7; i++){
+        var url = '../Graphic/model/sugarpoly*.gltf'
+	        loader.load(url.replace('*',String(i)), function(gltf){
+	            gltf.scene.children[0].scale.set(10,10,10);
+	            scene.add(gltf.scene);
+                rectangle.push(gltf.scene.children[0].id);
+                console.log(i);
+                mainShapeObject = gltf.scene.children[0];
+	        },  undefined, function (error) {
+                console.log(error);
+	    });
+    }
 }
 
 function onClick() {
@@ -75,12 +110,60 @@ function onClick() {
     raycaster.setFromCamera(mouse, camera);
     var intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
-        console.log('Intersection:', intersects[0]);
-        clicked = new THREE.Vector3(intersects[0].point.x,intersects[0].point.y,intersects[0].point.z);
-        cameraVecter = new THREE.Vector3(camera.position.x,camera.position.y,camera.position.z);
-        vectorToMove = clicked.sub(cameraVecter).normalize();
+        clickedPosition = new THREE.Vector3(intersects[0].point.x,intersects[0].point.y,intersects[0].point.z);
+        cameraPosition = new THREE.Vector3(camera.position.x,camera.position.y,camera.position.z);
+
+        vectorToMove = clickedPosition.sub(cameraPosition).normalize().multiplyScalar(20);
         objectToMove = intersects[0].object;
-        objectToMove.position.add(vectorToMove);
+
+        animationObject.forEach((object) =>{if(objectToMove.id == object.id)alreadyIn = true;});
+        if(!alreadyIn && !(objectToMove.id == mainShapeObject.id)){
+            animationObject.push(objectToMove);
+            animationDestinationVector.push(objectToMove.clone().position.add(vectorToMove.clone().normalize()).add(vectorToMove).ceil());
+            animationMovingVector.push(vectorToMove.normalize());
+        }
+        alreadyIn = false;
     }
+}
+
+function moveAnimation() {
+    animationObject.forEach((object,index) =>{
+        if (!animationDestinationVector[index].equals(object.clone().position.ceil())) {
+            object.position.add(animationMovingVector[index]);
+        }else{
+            animationObject[index].visible=false;
+            rectangle = rectangle.filter((element) => element != animationObject[index].id);
+            animationObject.splice(index,1);
+            animationDestinationVector.splice(index,1);
+            animationMovingVector.splice(index,1);
+        }
+    });
+}
+
+function rotateAnimation() {
+    animationObject.forEach((object) =>{
+        box = new THREE.Box3().setFromObject( object );
+        offset = new THREE.Vector3();
+        box.getCenter(offset);
+        if(Math.abs(offset.x)>Math.abs(offset.z)){
+            object.rotation.z -= 0.1;
+        }else{
+            object.rotation.x += 0.1;
+        }
+    });
+}
+
+function fadeOutAnimation() {
+    animationObject.forEach((object) =>{
+        object.material.transparent = true;
+        object.material.opacity -= 0.03;
+        object.material.format = THREE.RGBAFormat
+    });
+}
+
+function finallAnimation() {
+	camera.position.set(0,30,0);
+    camera.rotation.set(-1.5,0,0);
+    mainShapeObject.rotation.z -= 0.1;
 }
 
